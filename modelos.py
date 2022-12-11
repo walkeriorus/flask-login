5022ba215e90ab42b8f5ad35823fa76460423908
Walter Perez <walkeriorus472@gmail.com>
2022-12-11 00:36:11 -0300

    Agregado el archivo modelos.py, que agrega la clase Product

---
+++ modelos.py
@@ -0,0 +1,6 @@
+class Product:
+        def __init__(self, id, name, price, image):
+            self.id = id
+            self.name = name
+            self.price = price
+            self.image = image
